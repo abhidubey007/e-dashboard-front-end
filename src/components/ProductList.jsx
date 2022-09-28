dcc65b3253e656e9a7d4f56f019eca56d9944a5e
abhidubey007 <abhidb95@gmail.com>
2022-09-28 18:31:05 +0530

    Search field done

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -26,40 +26,59 @@ const ProductList = () => {
         }
     }
 
+    const searchHandler = async (e) => {
+        let key = e.target.value
+        if (key) {
+            let result = await fetch(`http://localhost:5000/search/${key}`)
+            result = await result.json()
+            if (result) {
+                setProducts(result)
+            }
+        }
+        else {
+            getProducts()
+        }
+    }
+
     return (
         <div className='form'>
             <h1>Product List</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>S.N.</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Brand</th>
-                        <th>Category</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                {
-                    products.length > 0 ? products.map((item, index) => {
-                        return (
-                            <tbody key={item._id}>
-                                <tr>
-                                    <td>{index + 1}</td>
-                                    <td>{item.name}</td>
-                                    <td>{item.price}</td>
-                                    <td>{item.company}</td>
-                                    <td>{item.category}</td>
-                                    <td>
-                                        <button><Link to={`/update/${item._id}`}>Update</Link></button>
-                                        <button onClick={() => deleteProduct(item._id)}>Delete</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        )
-                    }) : null
-                }
-            </table>
+            <input onChange={searchHandler} className='search-product-box' type="text" placeholder='Search Product' />
+            {
+                products.length > 0 ?
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>S.N.</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Brand</th>
+                                <th>Category</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        {
+                            products.map((item, index) => {
+                                return (
+                                    <tbody key={item._id}>
+                                        <tr>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.price}</td>
+                                            <td>{item.company}</td>
+                                            <td>{item.category}</td>
+                                            <td>
+                                                <button><Link to={`/update/${item._id}`}>Update</Link></button>
+                                                <button onClick={() => deleteProduct(item._id)}>Delete</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                )
+                            })
+                        }
+                    </table>
+                    : <h1>No Result Found</h1>
+            }
         </div>
     )
 }
